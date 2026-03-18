d69e20e76b5c1aefd0808dcf609cf56fdbca73dc
sevru <sevruliza@gmail.com>
2026-03-18 23:00:55 +1000

    todolist almost done

---
+++ Blazor/TODOlist/Classes/TODOitem.cs
@@ -0,0 +1,29 @@
+﻿namespace TODOlist.Classes
+{
+    public class TODOitem : IComparable<TODOitem>
+    {
+        public string Title { get; set; }
+        public bool DONE { get; set; }
+
+        //public static bool operator ==(TODOitem left, TODOitem right)
+        //{
+        //    return left.Title == right.Title;
+        //}
+        //public static bool operator !=(TODOitem left, toTODOitemdoitem right)
+        //{
+        //    return left.Title != right.Title;
+        //}
+        public int CompareTo(TODOitem other)
+        {
+            return Title.CompareTo(other.Title);
+        }
+        public bool Equals(TODOitem left, TODOitem right)
+        {
+            return left.Title == right.Title;
+        }
+        public int GetHashCode(TODOitem other)
+        {
+            return HashCode.Combine(Title);
+        }
+    }
+}
---
+++ Blazor/TODOlist/Components/Pages/TODO.razor.cs
@@ -0,0 +1,19 @@
+﻿using TODOlist.Classes;
+
+namespace TODOlist.Components.Pages
+{
+    public partial class TODO
+    {
+
+        HashSet<TODOitem> todos = [];
+        string task;
+        void AddTask() 
+        {
+            if (string.IsNullOrWhiteSpace(task)) return;
+            if (todos.Contains(new TODOitem { Title = task })) return;
+            todos.Add(new TODOitem { Title = task});
+            task = "";
+            
+        }
+    }
+}
