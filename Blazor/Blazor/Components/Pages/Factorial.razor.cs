c9ce8bac4b9b90b820d56452ff51d896b8abffe5
sevru <sevruliza@gmail.com>
2026-03-18 16:58:49 +1000

    Factorial BigInteger done

---
+++ Blazor/Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+namespace Blazor.Components.Pages
+{
+    public partial class Factorial
+    {
+        long number;
+        BigInteger factorial = 1;
+        List<BigInteger> results = new List<BigInteger>();
+        void setNumber(long number)
+        {
+            this.number = number;
+        }
+        void Calculate()
+        {
+            factorial = 1;
+            results = new List<BigInteger>();
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+                results.Add(factorial);
+            }
+        }
+    }
+}
