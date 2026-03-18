e7efbc8e71b06a393d401f063cb792504152fe60
sevru <sevruliza@gmail.com>
2026-03-18 17:28:35 +1000

    dz done

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        long number = 10;
+        BigInteger fibonacci = 55;
+        List<BigInteger> results = new();//оказывается можно и так и new List<BigInteger>()
+
+        void OnNumberChanged()
+        {
+            Calculate();
+        }
+
+        void Calculate()
+        {
+            results.Clear();
+
+            BigInteger a = 0;
+            BigInteger b = 1;
+
+            for (int i = 0; i <= number; i++)
+            {
+                results.Add(a);
+                BigInteger temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            fibonacci = results[^1];
+        }
+    }
+}
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        private double baseNum;
+        private double exponent;
+        private double result = 1;
+        private List<double> steps = new();
+
+        private void OnBaseChanged()
+        {
+            Calculate();
+        }
+
+        private void OnExponentChanged()
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            result = 1;
+            steps.Clear();
+
+            if (exponent < 0 || baseNum == 0) return;
+
+            for (int i = 1; i <= (int)exponent; i++)
+            {
+                result *= baseNum;
+                steps.Add(result);
+            }
+        }
+    }
+}
