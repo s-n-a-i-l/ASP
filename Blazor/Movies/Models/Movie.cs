78d45d4119d04147a95cc8684733dbe47dd53e50
sevru <sevruliza@gmail.com>
2026-03-22 00:01:57 +1000

    movie details & url

---
+++ Blazor/Movies/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100,MinimumLength =3)]
+        public string Title { get; set; } = string.Empty;
+
+        [RangeAttribute(typeof(DateOnly),"1888-10-14","9999-12-31")]
+        public DateOnly ReleaseDate { get; set; }
+        
+        public string Genre { get; set; } = string.Empty;
+        public string? URL { get; set; }
+
+    }
+}
