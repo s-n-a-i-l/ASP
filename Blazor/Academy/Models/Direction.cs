0edafcc766ab7e2dd08a229c9d873157425214b2
sevru <sevruliza@gmail.com>
2026-04-13 18:23:20 +1000

    isValid done

--- Blazor/Academy/Models/Direction.cs
+++ Blazor/Academy/Models/Direction.cs
@@ -1,35 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Academy.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Academy.Models 
 {
-    public class Direction :  IEquatable<Direction>, IComparable<Direction>
+    public class Direction 
     {
         [Key]//показывает, что поле ниже является ключ
         public byte direction_id { get; set; }
         [Required]
+        [UniqueDirectionName(ErrorMessage="Такое направление уже существует")]
         public string direction_name { get; set; }
-        //    public override bool Equals(object? other)
-        //    {
-        //        return this.direction_name.Equals((other as Direction).direction_name);
-        //    }
-        //    public override int GetHashCode()
-        //    {
-        //        return HashCode.Combine(direction_name);
-        //    }
         
-        public int CompareTo(Direction? other) // other не может быть null, поэтому "?" означает что параметр other может быть null
-        {
-            return other is null ? 1 : string.Compare(direction_name, other.direction_name);
-        }
-        public bool Equals(Direction? other)//как и тут  
-        {
-            if (other is null) return false;
-            return string.Equals(direction_name, other.direction_name, StringComparison.OrdinalIgnoreCase);
-        }
-        public override bool Equals(object? obj) => Equals(obj as Direction);
-        public override int GetHashCode()
-        {
-            return direction_name?.GetHashCode(StringComparison.OrdinalIgnoreCase) ?? 0;
-        }
+        
+        
     }
 }
