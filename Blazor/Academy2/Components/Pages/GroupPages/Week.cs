7cd44a94aa17f2381ef464e193594c4410b1f3da
sevru <sevruliza@gmail.com>
2026-05-04 18:29:34 +1000

    weekdays done

---
+++ Blazor/Academy2/Components/Pages/GroupPages/Week.cs
@@ -0,0 +1,23 @@
+﻿namespace Academy2.Components.Pages.GroupPages
+{
+    public class Week
+    {
+        readonly string[] DAYNAMES = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int days;
+        public Week(int days) 
+        {
+            this.days = days;
+        }
+        public override string ToString()
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < 7; i++)
+            {
+                if (((days >> i) & 1) != 0) result.Add(DAYNAMES[i]);
+            }
+
+            //return result;
+            return string.Join(", ", result);
+        }
+    }
+}
